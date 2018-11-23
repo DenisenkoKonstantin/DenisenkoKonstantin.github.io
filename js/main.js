8fa0fc16df970f7c629bcf1a037bb90555fa6987
KonstantinDenisenko <konstantin.denisenko91@gmail.com>
2018-11-23 17:08:38 +0200

    added file JS, added EventListener for mob_menu

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var navMobBtn = document.querySelector(".nav-mob");
+var backgroundMain = document.getElementById("opacity-main");
+var mobSideMenu = document.querySelector(".mob-side-menu");
+var mobMenuHiddenBtn = document.querySelector(".mob-menu-close");
+var sideMenuLiBtn = document.getElementById("sideMenuLi");
+var insideMenu = document.querySelector(".inside-menu");
+var startPage = document.querySelector(".page-1");
+
+navMobBtn.addEventListener("click", function () {
+    navMob();
+});
+
+backgroundMain.addEventListener("click", function () {
+    mobMenuHidden();
+});
+
+mobMenuHiddenBtn.addEventListener("click", function () {
+    mobMenuHidden();
+});
+
+sideMenuLiBtn.addEventListener("click", function () {
+    sideMenuLi();
+});
+
+function navMob() {
+    backgroundMain.classList.add("opacity-main");
+    mobSideMenu.style.right = 0;
+    startPage.style = "overflow: hidden"
+}
+
+function mobMenuHidden() {
+    backgroundMain.classList.remove("opacity-main");
+    mobSideMenu.style.right = -340 + "px";
+    insideMenu.style.height = 0;
+    startPage.style = "overflow: auto"
+}
+
+function sideMenuLi() {
+    if (insideMenu.style.height === "0px") {
+        insideMenu.style.height = 367 + "px";
+    }else {
+        insideMenu.style.height = 0;
+    };
+}
+
+
